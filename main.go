51b486df2aac700e2e19a249c9f3fd8209328049
dhinesh4668 <dhinesh4668@gmail.com>
2025-04-01 16:25:05 +0000

    docker config to go backend

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+// sample data
+
+type userDetails struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var users = []userDetails{
+	{ID: 1, Name: "dhinesh", Email: "dhinesh@samplemail.com", Password: "kjshdkfhskdfh"},
+	{ID: 2, Name: "dhinesh1", Email: "dhinesh1@samplemail.com", Password: "kjshdkfhskdfh"},
+}
+
+func getUsersData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(users)
+}
+
 func main() {
 	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/post", postHanduler)
+	// http.HandleFunc("/", handler)
+	// http.HandleFunc("/post", postHandler)
+
+	// get users data
+	http.HandleFunc("/users", getUsersData)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -34,9 +58,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
 
-// function for post handuler
-func postHanduler(w http.ResponseWriter, r *http.Request) {
-	
+// function for post handler
+func postHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "post server")
 }
-
