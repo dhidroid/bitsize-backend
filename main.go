38501725632b1a2e9f6fc884b65fb2fe578e5b8c
dhinesh4668 <dhinesh4668@gmail.com>
2025-04-04 16:33:45 +0000

    update code add welocme handuler

--- main.go
+++ main.go
@@ -1,40 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-// sample data
-
-type userDetails struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-var users = []userDetails{
-	{ID: 1, Name: "dhinesh", Email: "dhinesh@samplemail.com", Password: "kjshdkfhskdfh"},
-	{ID: 2, Name: "dhinesh1", Email: "dhinesh1@samplemail.com", Password: "kjshdkfhskdfh"},
-}
-
-func getUsersData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(users)
-}
-
 func main() {
-	log.Print("starting server...")
-	// http.HandleFunc("/", handler)
-	// http.HandleFunc("/post", postHandler)
-
-	// get users data
-	http.HandleFunc("/users", getUsersData)
+	log.Print("bitsSize starting server...")
+	http.HandleFunc("/welcome", welcomeHanduler)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -50,15 +25,6 @@ func main() {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
-}
-
-// function for post handler
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "post server")
+func welcomeHanduler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome our bits learn application")
 }
