d25b9a2d9d04ae3f8a8038a99da6bef9d4aa1dd1
dhinesh4668 <dhinesh4668@gmail.com>
2025-04-18 12:58:57 +0000

    gin package update

--- main.go
+++ main.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"bitsize-backend/config"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	serverPort := config.AppConfig.GetPort()
-	// Set up HTTP handlers
-	http.HandleFunc("/welcome", welcomeHandler)
-	http.HandleFunc("/", helloHandler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = serverPort
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to our bits learn application")
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello user , welcome to our app !")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Go Gin API!",
+		})
+	})
+	r.Run(":3000")
 }
